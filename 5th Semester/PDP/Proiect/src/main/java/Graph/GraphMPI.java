5ce3ba608e3d8b0120e9b31d16e128f03e92c3c1
raulianos09 <raul_ianos@yahoo.com>
2022-01-10 21:43:44 +0200

    update

---
+++ 5th Semester/PDP/Proiect/src/main/java/Graph/GraphMPI.java
@@ -0,0 +1,116 @@
+package Graph;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class GraphMPI {
+
+    private int numberOfEdges;
+
+    public int getNumberOfVertices() {
+        return numberOfVertices;
+    }
+
+    private int numberOfVertices;
+
+    private Set<Integer> colors;
+    private Map<GNode, Set<GNode>> graph;
+
+    public Set<Integer> getIndependentSet() {
+        return independentSet.stream().map(n->n.getNodeID()).collect(Collectors.toSet());
+    }
+
+    private final Set<GNode> independentSet;
+    private ExecutorService executorService;
+
+    public GraphMPI(int numberOfVertices) {
+        this.numberOfVertices = numberOfVertices;
+        this.numberOfEdges = numberOfEdges;
+
+        graph = new HashMap<>();
+
+        for (int i = 0; i < numberOfVertices; i++) {
+            GNode n = new GNode(i);
+            n.setColor(-1);
+            Random r = new Random();
+            n.setWeight(r.nextInt(0, 100));
+            graph.put(n, new HashSet<>());
+        }
+
+        independentSet = new HashSet<>(graph.keySet());
+        executorService = Executors.newFixedThreadPool(8);
+    }
+
+    public void setColor(GNode node) {
+        Set<GNode> neighbours = this.graph.get(node);
+        Set<Integer> colors = neighbours.stream().map(n -> n.getColor()).collect(Collectors.toSet());
+        int maxColor = Collections.max(colors) + 1;
+        for (int i = 0; i <= maxColor; i++)
+            if (!colors.contains(i))
+                node.setColor(i);
+    }
+
+    public List<Integer> toMatrix() {
+        List<Integer> matrix = new ArrayList<>();
+        for (int i = 0; i < numberOfVertices; i++)
+            for (int j = 0; j < numberOfVertices; j++)
+                matrix.add(0);
+
+        for (GNode node : graph.keySet())
+            for (GNode neighbour : graph.get(node)) {
+                matrix.set(node.getNodeID()*numberOfVertices + neighbour.getNodeID(),1);
+            }
+        return matrix;
+    }
+
+    public void addEdge(int node1, int node2) {
+        GNode n1 = this.graph.keySet().stream().filter(gNode -> gNode.getNodeID() == node1).collect(Collectors.toList()).get(0);
+        GNode n2 = this.graph.keySet().stream().filter(gNode -> gNode.getNodeID() == node2).collect(Collectors.toList()).get(0);
+        this.graph.get(n1).add(n2);
+        this.graph.get(n2).add(n1);
+    }
+
+
+    public void show() {
+        List<String> colors = Arrays.asList("#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+                "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+                "#5A0007", "#809693", "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+                "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+                "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+                "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+                "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+                "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+                "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+                "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+                "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+                "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+                "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C");
+        Collections.shuffle(colors);
+        System.setProperty("org.graphstream.ui", "swing");
+        org.graphstream.graph.Graph graph = new SingleGraph("visual_graph");
+        graph.setAttribute("ui.stylesheet", "node {size: 30px;}");
+        graph.setAttribute("ui.antialias");
+        for (GNode node : this.graph.keySet()) {
+            Node n = graph.addNode(Integer.toString(node.getNodeID()));
+            n.setAttribute("ui.style", "fill-color:" + colors.get(node.getColor() % colors.size()) + "; text-size: 20px;");
+            n.setAttribute("ui.size", "3gu");
+            n.setAttribute("ui.label", Integer.toString(node.getNodeID()));
+            n.setAttribute("ui.class", "big");
+        }
+        for (GNode node : this.graph.keySet()) {
+            for (GNode node2 : this.graph.get(node)) {
+                if (node2.getNodeID() < node.getNodeID())
+                    continue;
+                Edge e = graph.addEdge(Integer.toString(node.getNodeID()) + Integer.toString(node2.getNodeID()), Integer.toString(node.getNodeID()), Integer.toString(node2.getNodeID()));
+                e.setAttribute("ui.style", "size: 3px;");
+            }
+        }
+        graph.display();
+    }
+}
--- 5th Semester/PDP/Proiect/src/main/java/Main.java
+++ 5th Semester/PDP/Proiect/src/main/java/Main.java
@@ -1,24 +1,133 @@
 import Graph.*;
+import mpi.MPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        Graph g = new Graph(10);
-        g.addEdge(0, 2);
-        g.addEdge(0, 3);
-        g.addEdge(0, 5);
-        g.addEdge(1, 3);
-        g.addEdge(1, 4);
-        g.addEdge(1, 6);
-        g.addEdge(2, 4);
-        g.addEdge(2, 7);
-        g.addEdge(3, 8);
-        g.addEdge(4, 9);
-        g.addEdge(5, 6);
-        g.addEdge(5, 9);
-        g.addEdge(6, 7);
-        g.addEdge(7, 8);
-        g.addEdge(8, 9);
-        g.colorGraph();
-        g.show();
+
+        String APPROACH = "MPI";
+
+        if (APPROACH.equals("MPI")) {
+
+            MPI.Init(args);
+
+            int rank = MPI.COMM_WORLD.Rank();
+            int numberOfProcesses = MPI.COMM_WORLD.Size();
+
+            GraphMPI g = new GraphMPI(10);
+            g.addEdge(0, 2);
+            g.addEdge(0, 3);
+            g.addEdge(0, 5);
+            g.addEdge(1, 3);
+            g.addEdge(1, 4);
+            g.addEdge(1, 6);
+            g.addEdge(2, 4);
+            g.addEdge(2, 7);
+            g.addEdge(3, 8);
+            g.addEdge(4, 9);
+            g.addEdge(5, 6);
+            g.addEdge(5, 9);
+            g.addEdge(6, 7);
+            g.addEdge(7, 8);
+            g.addEdge(8, 9);
+
+            if (rank == 0) {
+                master(g, numberOfProcesses);
+            } else {
+                worker(rank);
+            }
+
+            //g.show();
+
+            MPI.Finalize();
+
+        } else {
+            Graph g = new Graph(10);
+            g.addEdge(0, 2);
+            g.addEdge(0, 3);
+            g.addEdge(0, 5);
+            g.addEdge(1, 3);
+            g.addEdge(1, 4);
+            g.addEdge(1, 6);
+            g.addEdge(2, 4);
+            g.addEdge(2, 7);
+            g.addEdge(3, 8);
+            g.addEdge(4, 9);
+            g.addEdge(5, 6);
+            g.addEdge(5, 9);
+            g.addEdge(6, 7);
+            g.addEdge(7, 8);
+            g.addEdge(8, 9);
+            g.colorGraph();
+            g.show();
+        }
+    }
+
+    private static void worker(int rank) {
+        Object[] matrix = new Object[1];
+        int[] begin = new int[1];
+        int[] end = new int[1];
+        int[] numberOfVertices = new int[1];
+
+        MPI.COMM_WORLD.Recv(matrix, 0, 1, MPI.OBJECT, 0, 0);
+
+        String stringMatrix = (String) matrix[0];
+        List<Integer> linearizedMatrix = createListFromString(stringMatrix.substring(1, stringMatrix.length() - 1));
+        List<List<Integer>> actualMatrix = new ArrayList<>();
+
+        MPI.COMM_WORLD.Recv(numberOfVertices, 0, 1, MPI.INT, 0, 0);
+        for (int i = 0; i < numberOfVertices[0]; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < numberOfVertices[0]; j++) {
+                row.add(linearizedMatrix.get(i * numberOfVertices[0] + j));
+            }
+            actualMatrix.add(row);
+        }
+
+
+
+        MPI.COMM_WORLD.Recv(begin, 0, 1, MPI.INT, 0, 0);
+        MPI.COMM_WORLD.Recv(end, 0, 1, MPI.INT, 0, 0);
+
+
+    }
+
+    private static void master(GraphMPI g, int numberOfProcesses) {
+        int start = 0, finish = 0;
+        int len = g.getNumberOfVertices() / (numberOfProcesses - 1);
+
+        MPI.COMM_WORLD.Bcast(g,0,1,MPI.OBJECT,0);
+
+        for (int i = 1; i < numberOfProcesses; i++) {
+            start = finish;
+            finish += len;
+
+            if (i == numberOfProcesses - 1) {
+                finish = g.getNumberOfVertices();
+            }
+
+
+            MPI.COMM_WORLD.Send(new Object[]{g.toMatrix().toString()}, 0, 1, MPI.OBJECT, i, 0);
+            MPI.COMM_WORLD.Send(new int[]{g.getNumberOfVertices()}, 0, 1, MPI.INT, i, 0);
+
+
+            MPI.COMM_WORLD.Send(new int[]{start}, 0, 1, MPI.INT, i, 0);
+            MPI.COMM_WORLD.Send(new int[]{finish}, 0, 1, MPI.INT, i, 0);
+        }
+    }
+
+    private static List<Integer> createListFromString(String stringElements) {
+        List<String> listOfStringElems = Arrays.asList(stringElements.split(","));
+        List<Integer> actualList = new ArrayList<>();
+        for (String elem : listOfStringElems) {
+            actualList.add(Integer.parseInt(elem.trim()));
+        }
+        return actualList;
     }
 }
