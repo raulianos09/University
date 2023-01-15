6900cd537574ad075ec8c45a7d1aad9cc844fd7d
Raul Ianos <raul_ianos@yahoo.com>
2023-01-15 21:26:08 +0200

    small refactor

--- 5th Semester/CVDL/Project/Main.py
+++ 5th Semester/CVDL/Project/Main.py
@@ -13,20 +13,7 @@ if __name__ == "__main__":
   model = Model()
   inference = Inference()
 
-  #model.train_model()
-
-  #Load images for inference
+  model.train_model()
 
   gui = GUI(model,db,inference)
-
-  # for file in os.listdir(Utils.INFERENCE_IMAGES_PATH):
-  #   image_path = os.path.join(Utils.INFERENCE_IMAGES_PATH, file)
-  #   image = cv2.imread(image_path)
-  #   landmarks = db.get_landmarks_for_image(image)
-  #   if landmarks is not None:
-  #     aproximation = inference.interpret_landmarks(landmarks)
-  #     print("Regarding to the model your image falls into category:" ,aproximation)
-  #     db.show_image_landmarks(image)
-  #   else:
-  #     print("Gogu could not recognize a hand")
 
